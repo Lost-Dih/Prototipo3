9d5049732601da2b78d8bcb55a7899e28efc2e59
Diana <dfranciscamagalhaes@gmail.com>
2026-06-16 22:29:10 +0100

    Lesson 3.1 - Jump Force

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    private void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
